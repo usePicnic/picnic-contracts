73e46a13a223aa788be93dd43ac350693c6c5382
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-06-23 17:32:50 -0300

    [FEATURE] Adding coverage to NFT requires

--- test/NFTs.ts
+++ test/NFTs.ts
@@ -7,6 +7,7 @@ describe("NFTs", function () {
     let Pool;
     let hardhatPool;
     let owner;
+    let addr1;
     let pool721;
 
     const UNI_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
@@ -17,7 +18,7 @@ describe("NFTs", function () {
     let tokens = [UNI_TOKEN, ONEINCH_TOKEN]
 
     beforeEach(async function () {
-        [owner] = await ethers.getSigners()
+        [owner, addr1] = await ethers.getSigners()
 
         // Get the ContractFactory
         Pool = await ethers.getContractFactory("Pool");
@@ -25,12 +26,12 @@ describe("NFTs", function () {
         // To deploy our contract, we just have to call Pool.deploy() and await
         // for it to be deployed(), which happens onces its transaction has been
         // mined.
-        hardhatPool = (await Pool.deploy(UNI_ROUTER)).connect(owner);        
+        hardhatPool = (await Pool.deploy(UNI_ROUTER)).connect(owner);
 
         pool721 = await ethers.getContractAt(
             "Pool721",
             await hardhatPool.get_pool721_address()
-            );
+        );
 
         await hardhatPool.create_index(
             tokens.map(() => 1000000000),  // uint256[] _allocation,
@@ -58,9 +59,15 @@ describe("NFTs", function () {
             tokens.map(x => [WETH, x]), // paths
             overrides
         );
+
+        await hardhatPool.create_index(
+            tokens.map(() => 1000000000),  // uint256[] _allocation,
+            tokens, // address[] _tokens
+            tokens.map(x => [x, WETH]), // paths
+        );
     });
 
-    it("Cash out from index is working :D", async () => {
+    it("Mint token is working :D", async () => {
         // All balances sshould live inside the contract
         await expect(await hardhatPool.get_token_balance(0, UNI_TOKEN, owner.getAddress())).to.above(0);
         await expect(await hardhatPool.get_token_balance(1, UNI_TOKEN, owner.getAddress())).to.above(0);
@@ -75,30 +82,46 @@ describe("NFTs", function () {
         await hardhatPool.mint_Pool721(1);
         await expect(await pool721.balanceOf(owner.address)).to.be.equal(2);
     });
-    
-    it("Cash out and burn token", async () => {
+
+    it("Rejects mint if there is no deposit", async () => {    
+        await expect(
+            hardhatPool.mint_Pool721(2)
+        ).to.be.revertedWith("NOT ENOUGH FUNDS");
+    });
+
+    it("Burn token", async () => {
         await expect(await hardhatPool.get_token_balance(0, UNI_TOKEN, owner.getAddress())).to.above(0);
         await hardhatPool.mint_Pool721(0);
-                
+
         await expect(await pool721.balanceOf(owner.address)).to.be.equal(1);
         await expect(await hardhatPool.get_token_balance(0, UNI_TOKEN, owner.getAddress())).to.equal(0);
-        
+
         await hardhatPool.burn_Pool721(0);
 
         await expect(await pool721.balanceOf(owner.address)).to.be.equal(0);
         await expect(await hardhatPool.get_token_balance(0, UNI_TOKEN, owner.getAddress())).to.above(0);
     });
-    
+
+    it("Rejects burn token when called by someone who is not the token owner", async () => {
+        let contractAsSigner0 = hardhatPool.connect(addr1);
+
+        await hardhatPool.mint_Pool721(0);
+
+        await expect(
+            contractAsSigner0.burn_Pool721(0)
+        ).to.be.revertedWith("ONLY CALLABLE BY TOKEN OWNER");
+    });
+
     it("Rejects calls from other addresses", async () => {
-        await expect( pool721.generatePool721(
+        await expect(pool721.generatePool721(
             owner.getAddress(),
             0, // index_id
             [2, 3] // allocation
-          )).to.be.revertedWith("ONLY INDEXPOOL CAN CALL THIS FUNCTION");
+        )).to.be.revertedWith("ONLY INDEXPOOL CAN CALL THIS FUNCTION");
 
-          await expect(pool721.burnPool721(
+        await expect(pool721.burnPool721(
             0
-          )).to.be.revertedWith("ONLY INDEXPOOL CAN CALL THIS FUNCTION");
-       
-    });    
+        )).to.be.revertedWith("ONLY INDEXPOOL CAN CALL THIS FUNCTION");
+
+    });
 });
