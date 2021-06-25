2254198060a555a2c2727d8396255f13c8bd7cef
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-06-25 16:07:43 -0300

    [FEATURE] Increasing coverage of OraclePair

--- test/OraclePair.ts
+++ test/OraclePair.ts
@@ -10,6 +10,8 @@ describe("OraclePair", function () {
   const UNI_FACTORY = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
   const UNI_TOKEN = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984";
   const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  const SUSHI_TOKEN = "0x6b3595068778dd592e39a122f4f5a5cf09c90fe2";
+
 
   beforeEach(async function () {
     [owner] = await ethers.getSigners();
@@ -43,6 +45,19 @@ describe("OraclePair", function () {
     oracle.update();
 
     expect(await oracle.consult(UNI_TOKEN, 1000000)).to.be.above(0);    
+  }) 
+
+  it("Rejects consult of token outside of pair", async function () {   
+    let Oracle = await ethers.getContractFactory("OraclePair");
+
+    oracle = (await Oracle.deploy(UNI_FACTORY, UNI_TOKEN, WETH)).connect(owner);
+
+    // suppose the current block has a timestamp of 01:00 PM
+    await hre.network.provider.send("evm_increaseTime", [10800])
+    await hre.network.provider.send("evm_mine") // this one will have 02:00 PM as its timestamp 
+    
+    oracle.update();
+    await expect(oracle.consult(SUSHI_TOKEN, 1000000)).to.be.revertedWith('ExampleOracleSimple: INVALID_TOKEN');
   }) 
 })
 
