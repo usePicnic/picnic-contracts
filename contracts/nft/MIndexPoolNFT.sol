3a26148a45588425a5203ec45f2c3fd6ffe64ebe
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-06-28 15:41:49 -0300

    [FEATURE] Monolith compiling successfully :D

---
+++ contracts/nft/MIndexPoolNFT.sol
@@ -0,0 +1,61 @@
+pragma solidity 0.8.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "../interfaces/IIndexPoolNFT.sol";
+
+
+contract MIndexPoolNFT is ERC721, Ownable {
+    uint256 public tokenCounter;
+    address creator;
+    mapping(uint256 => address[]) public tokenIdToTokenAddresses;
+    mapping(uint256 => uint256[]) public tokenIdToAllocation;
+    mapping(uint256 => uint256) public tokenIdToIndexId;
+
+    constructor(address _creator) public ERC721("Dogie", "DOG") {
+        tokenCounter = 0;
+        creator = _creator;
+    }
+
+    modifier _indexpoolOnly_() {
+        require(msg.sender == creator, "ONLY INDEXPOOL CAN CALL THIS FUNCTION");
+        _;
+    }
+
+    function generatePool721(
+        address user,
+        uint256 indexId,
+        uint256[] memory allocation
+    ) external _indexpoolOnly_ returns (uint256) {
+        uint256 newItemId = tokenCounter;
+
+        _safeMint(user, newItemId);
+
+        tokenIdToIndexId[newItemId] = indexId;
+        tokenIdToAllocation[newItemId] = allocation;
+
+        tokenCounter = tokenCounter + 1;
+
+        return newItemId;
+    }
+
+    function burnPool721(
+        uint256 tokenId
+    ) external _indexpoolOnly_ returns (uint256, uint256[] memory) {
+        uint256 indexId = tokenIdToIndexId[tokenId];
+        uint256[] memory allocation = tokenIdToAllocation[tokenId];
+
+        _burn(tokenId);
+
+        return (indexId, allocation);
+    }
+
+    function viewPool721(
+        uint256 tokenId
+    ) external view returns (uint256, uint256[] memory)  {
+        uint256 indexId = tokenIdToIndexId[tokenId];
+        uint256[] memory allocation = tokenIdToAllocation[tokenId];
+
+        return (indexId, allocation);
+    }
+}
