fedd268704a4333728a761d755dfafd9a9283df0
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-07-02 17:42:08 -0300

    [BUG] Setting chain ID properly

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -42,9 +42,9 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
+      chainId: 7671,
       forking: {
         url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_KEY}`, 
-        chainId: 7671
       }
     },
   },
