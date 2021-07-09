f28ee07841e1fef3c1b876af0c85a1b3fa41e5c2
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-07-09 10:05:24 -0300

    [BUG] Fixing path order in order to estimate amounts in ETH

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -240,7 +240,6 @@ contract Pool is IPool {
         for (uint8 i = 0; i < allocation.length; i++) {
             // Set temp variables
             path = paths[i];
-            address[] memory invPath = new address[](path.length);
             tokenAddress = tokens[i];
 
             if (address(0) != tokenAddress) {
@@ -257,6 +256,7 @@ contract Pool is IPool {
                 );
 
                 // Calculate inverse path and instantiate Oracles
+                address[] memory invPath = new address[](path.length);
                 for (uint8 j = 0; j < path.length; j++) {
                     invPath[path.length - 1 - j] = path[j];
                 }
@@ -374,14 +374,19 @@ contract Pool is IPool {
                 "WRONG PATH: TOKEN NEEDS TO BE PART OF PATH"
             );
 
+            address[] memory invPath = new address[](path.length);
+            for (uint8 j = 0; j < path.length; j++) {
+                invPath[path.length - 1 - j] = path[j];
+            }
+
             if (address(0) == tokens[i]) {
                 amount = allocation[i];
             } else {
-                _oracle.updateOracles(path);
-                amount = _oracle.consult(path);
+                _oracle.updateOracles(invPath);
+                amount = _oracle.consult(invPath);
 
                 if (amount == 0) {
-                    amount = _uniswapRouter.getAmountsOut(allocation[i], path)[
+                    amount = _uniswapRouter.getAmountsOut(allocation[i], invPath)[
                     path.length - 1
                     ];
                 }
