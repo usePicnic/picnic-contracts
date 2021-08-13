471c0379c791abac789d52c9c13ac899481cbad3
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-08-13 18:00:06 -0300

    [BUG] Deploy logic working fine :D

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,15 @@
 import deployIndexPool from "./utils/deployIndexPool";
+import deployBridge from "./utils/deployBridge";
 
+const {readdirSync} = require('fs')
 
 async function main() {
     await deployIndexPool();
+
+    const files = readdirSync('./contracts/bridges/');
+    for (var i =0; i < files.length; i++){
+        await deployBridge(files[i].replace('.sol', ''))
+    }
 }
 
 main()
--- scripts/utils/deployLogic.ts
+++ scripts/utils/deployLogic.ts
@@ -4,8 +4,23 @@ import { readFileSync } from "fs";
 import {MongoClient} from 'mongodb';
 
 async function deployLogic(contractName: string, isBridge: boolean) {
+    console.log(contractName)
+
+    const uri = process.env.MONGODB_URI;
+    // @ts-ignore
+    const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+    await client.connect();
+
+    // TODO add logic to support multiple blockchains
+    let mongoContract = await client.db('indexpool').collection('contracts').findOne({'name':contractName});
+
+    if (mongoContract !== undefined){
+        console.log(`${contractName} is already deployed. If you want to redeploy it, please undeploy it first.`)
+        return
+    }
+
     console.log(`Deploying ${contractName}:`);
-    let contractInterface = await ethers.getContractFactory('AaveV2DepositBridge');
+    let contractInterface = await ethers.getContractFactory(contractName);
     const deployedContract = await contractInterface.deploy();
 
     console.log(`${contractName} contract deployed at: ${deployedContract.address}`);
@@ -23,12 +38,6 @@ async function deployLogic(contractName: string, isBridge: boolean) {
         'utf8')
     const contract = JSON.parse(contractFile)
 
-    const uri = process.env.MONGODB_URI;
-
-    // @ts-ignore
-    const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-
-    await client.connect();
     let network = await client.db('indexpool').collection('networks').findOne({'name':'polygon'});
 
     let insertData = {
