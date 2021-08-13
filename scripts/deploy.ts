4ac08412b1c4c8c5f8e22140f26572f0ca024433
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-08-13 13:30:38 -0300

    [REFACTOR] Setting up a new deploy architecture for IndexPool :D

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,76 +4,56 @@
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 // const hre = require("hardhat");
-import { ethers } from "hardhat";
-import { readFileSync } from "fs";
+import {ethers} from "hardhat";
+import {readFileSync} from "fs";
 import fetch from "node-fetch";
 import constants from "../constants";
 
 require('dotenv').config()
 
 function delay(ms) {
-  return new Promise( resolve => setTimeout(resolve, ms) );
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  console.log(process.env.HI);
-  const [deployer] = await ethers.getSigners();
-  // const ADDRESSES = constants['POLYGON'];
+    console.log(process.env.HI);
+    const [deployer] = await ethers.getSigners();
+    // const ADDRESSES = constants['POLYGON'];
 
-  console.log(
-    "Deploying contracts with the account:",
-    deployer.address
-  );
+    console.log(
+        "Deploying contracts with the account:",
+        deployer.address
+    );
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+    console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // console.log("Deploying IndexPool contract");
-  // const IndexPool = await ethers.getContractFactory("IndexPool");
-  // const indexPool = await IndexPool.deploy();
+    console.log("Deploying IndexPool contract");
+    const IndexPool = await ethers.getContractFactory("IndexPool");
+    const indexPool = await IndexPool.deploy();
 
-  // console.log(indexPool.address);
+    console.log(`IndexPool contract deployed at: ${indexPool.address}`);
 
-  // deployer.
+    const indexPoolFile = readFileSync('./artifacts/contracts/IndexPool.sol/IndexPool.json', 'utf8')
+    const indexPoolContract = JSON.parse(indexPoolFile)
 
-  // console.log("Account balance:", (await deployer.getBalance()).toString());
-
-  // // DEPLOY
-  // const pool = await Pool.deploy(ADDRESSES['ROUTER'], oracle.address);
-
-  // console.log("Pool address:", pool.address);
-
-  // const nftAddress = await pool.getPortfolioNFTAddress();
-
-  // console.log("NFT Address", nftAddress)
-
-  // // REGISTER ON SERVER
-  // const poolFile = readFileSync('./artifacts/contracts/Pool.sol/Pool.json', 'utf8')
-  // const poolContract = JSON.parse(poolFile)
-
-  // const nftFile = readFileSync('./artifacts/contracts/nft/IndexPoolNFT.sol/IndexPoolNFT.json', 'utf8')
-  // const nftContract = JSON.parse(nftFile)
-
-  // const response = await fetch(
-  //   'https://indexpool-appservice.azurewebsites.net/api/setcontract?code=yKjNRKdRLV4xl5fdmZU6bPveAg6lxpmxmRB3ocSXYTkrCUyXIa3QgA%3D%3D', {
-  //     method: 'post',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({
-  //       networkName: "polygon-testnet",
-  //       address: pool.address,
-  //       abi: poolContract['abi'],
-  //       nftAddress: nftAddress,
-  //       nftAbi: nftContract['abi']
-  //     })
-  //   })
-  // const responseText = await response.text()
-  // console.log(responseText)
+    const response = await fetch(
+        'https://indexpool-appservice.azurewebsites.net/api/setcontract?code=yKjNRKdRLV4xl5fdmZU6bPveAg6lxpmxmRB3ocSXYTkrCUyXIa3QgA%3D%3D', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                networkName: "polygon",
+                address: indexPool.address,
+                abi: indexPoolContract['abi'],
+            })
+        })
+    console.log(`MongoDB updated with new IndexPool contract values`);
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
