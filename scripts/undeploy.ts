bc299cfa838c8f1ba2836e207474327ff3178df8
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-08-13 18:32:41 -0300

    [FEATURE] Adding brute force undeploy script 😅

--- scripts/undeploy.ts
+++ scripts/undeploy.ts
@@ -1,6 +1,17 @@
+import {MongoClient} from 'mongodb';
 
 async function main() {
-  console.log("To be implemented");
+  console.log("Undeploying all contracts");
+  const uri = process.env.MONGODB_URI;
+  // @ts-ignore
+  const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+  await client.connect();
+
+  // TODO should we create a backup of what is being undeployed?
+  //  ... set active to false and change name to not conflict with unique index
+  // TODO filter what will be undeployed
+  await client.db('indexpool').collection('contracts').deleteMany({});
+  console.log("Undeploy is done :D");
 }
 
 main()
