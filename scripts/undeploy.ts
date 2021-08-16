2a263760da2e90aff4fe688cbec944ead11187ef
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-08-16 14:26:37 -0300

    [BUG] Reverting to working undeploy file 😅

--- scripts/undeploy.ts
+++ scripts/undeploy.ts
@@ -1,11 +1,22 @@
+import {MongoClient} from 'mongodb';
 
 async function main() {
-  console.log("To be implemented");
+  console.log("Undeploying all contracts");
+  const uri = process.env.MONGODB_URI;
+  // @ts-ignore
+  const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+  await client.connect();
+
+  // TODO should we create a backup of what is being undeployed?
+  //  ... set active to false and change name to not conflict with unique index
+  // TODO filter what will be undeployed
+  await client.db('indexpool').collection('contracts').deleteMany({});
+  console.log("Undeploy is done :D");
 }
 
 main()
-.then(() => process.exit(0))
-.catch(error => {
-  console.error(error);
-  process.exit(1);
-});
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
