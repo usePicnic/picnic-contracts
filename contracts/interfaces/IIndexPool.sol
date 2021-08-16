4e2f641f346fc2a551b01b6e72a34c507f5512f2
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-08-16 12:06:50 -0300

    [FEATURE] Adding IndexPool interface

---
+++ contracts/interfaces/IIndexPool.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.6;
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+pragma experimental ABIEncoderV2; // TODO is this necessary?
+
+interface IIndexPool is IERC721 { // TODO should we add interface for ownable?
+    function setMaxDeposit(uint256 newMaxDeposit) external;
+    function registerPortfolio(string calldata jsonString) external;
+    function mintPortfolio(
+        address finder,
+        address creator,
+        address[] calldata inputTokens,
+        uint256[] calldata inputAmounts,
+        address[] calldata _bridgeAddresses,
+        bytes[] calldata _bridgeEncodedCalls
+    ) external payable;
+    function editPortfolio(
+        uint256 nftId,
+        address finder,
+        address creator,
+        address[] calldata inputTokens,
+        uint256[] calldata inputAmounts,
+        address[] calldata _bridgeAddresses,
+        bytes[] calldata _bridgeEncodedCalls
+    ) external payable;
+}
+
+
