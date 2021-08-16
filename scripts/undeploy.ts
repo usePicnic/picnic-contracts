63c8c5cb5f9e115c37466fa0b04128658f22450a
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-08-16 14:29:21 -0300

    [BUG] Reverting back to Pedro's Pedro 😅

--- scripts/undeploy.ts
+++ scripts/undeploy.ts
@@ -1,22 +1,51 @@
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
+const hre = require("hardhat");
+const prompts = require("prompts");
 
 async function main() {
-  console.log("Undeploying all contracts");
-  const uri = process.env.MONGODB_URI;
-  // @ts-ignore
-  const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-  await client.connect();
+    const networkName = hre.hardhatArguments.network;
+    const response = await prompts(
+        [
+            {
+                type: 'text',
+                name: 'confirm1',
+                message: `Are you sure you want to undeploy contracts from ${networkName}? Type Guybrush Threepwood to confirm.`,
+                initial: ''
+            },
+            {
+                type: 'confirm',
+                name: 'confirm2',
+                message: `Really really?`,
+                initial: false
+            }
+        ]
+    )
+    if (response.confirm1 != 'Guybrush Threepwood' || !response.confirm2) {
+        console.log('Aborted');
+        return;
+    }
 
-  // TODO should we create a backup of what is being undeployed?
-  //  ... set active to false and change name to not conflict with unique index
-  // TODO filter what will be undeployed
-  await client.db('indexpool').collection('contracts').deleteMany({});
-  console.log("Undeploy is done :D");
+    console.log(`Undeploying all contracts from ${networkName}`);
+
+    const client = await MongoClient.connect(process.env.MONGODB_URI);
+    await client.connect();
+
+    // TODO should we create a backup of what is being undeployed?
+    //  ... set active to false and change name to not conflict with unique index
+    await client
+        .db('indexpool')
+        .collection('contracts')
+        .deleteMany(
+            {
+                networkName: networkName
+            }
+        );
+    console.log("Undeploy is done :D");
 }
 
 main()
     .then(() => process.exit(0))
     .catch(error => {
-      console.error(error);
-      process.exit(1);
+        console.error(error);
+        process.exit(1);
     });
