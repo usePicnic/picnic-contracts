50aee0c45e3b099fbf6421b8669fe3bdefba2466
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-08-16 14:39:11 -0300

    [FEATURE] Adding check to avoid blank network 😅

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,6 @@
 import deployLogic from "./utils/deployLogic";
-import { ethers } from "hardhat";
-import { BigNumber } from "ethers";
+import {ethers} from "hardhat";
+import {BigNumber} from "ethers";
 
 const hre = require("hardhat");
 const prompts = require("prompts");
@@ -14,25 +14,37 @@ const weiToString = (wei) => {
 }
 
 const contractsToDeploy = [
-    { contractName: "IndexPool", filePath: "./artifacts/contracts/IndexPool.sol/IndexPool.json" },
-    { contractName: "AaveV2DepositBridge", filePath: "./artifacts/contracts/bridges/AaveV2DepositBridge.sol/AaveV2DepositBridge.json" },
-    { contractName: "UniswapV2SwapBridge", filePath: "./artifacts/contracts/bridges/UniswapV2SwapBridge.sol/UniswapV2SwapBridge.json" },
+    {contractName: "IndexPool", filePath: "./artifacts/contracts/IndexPool.sol/IndexPool.json"},
+    {
+        contractName: "AaveV2DepositBridge",
+        filePath: "./artifacts/contracts/bridges/AaveV2DepositBridge.sol/AaveV2DepositBridge.json"
+    },
+    {
+        contractName: "UniswapV2SwapBridge",
+        filePath: "./artifacts/contracts/bridges/UniswapV2SwapBridge.sol/UniswapV2SwapBridge.json"
+    },
 ]
+
 async function main() {
     const networkName = hre.hardhatArguments.network;
+
+    if (networkName === undefined) {
+        console.log('Please set a network before deploying :D');
+        return;
+    }
 
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
 
     const balanceBegin = await deployer.getBalance();
-    console.log("Account balance:",weiToString(balanceBegin));
+    console.log("Account balance:", weiToString(balanceBegin));
 
     const response = await prompts({
-        type: 'confirm',
-        name: 'confirm',
-        message: `Are you sure you want to deploy to ${networkName}?`,
-        initial: false
-      }
+            type: 'confirm',
+            name: 'confirm',
+            message: `Are you sure you want to deploy to ${networkName}?`,
+            initial: false
+        }
     )
 
     console.log(response.confirm)
@@ -40,17 +52,17 @@ async function main() {
         console.log("Aborting");
         return;
     }
-  
+
     for (const c of contractsToDeploy) {
-        await deployLogic( {
+        await deployLogic({
             networkName: networkName,
             contractName: c.contractName,
             filePath: c.filePath
-        } )
+        })
     }
     const balanceEnd = await deployer.getBalance();
-    console.log("Account balance:",weiToString(balanceEnd));
-    console.log("Cost to deploy:",weiToString(balanceBegin.sub(balanceEnd)));
+    console.log("Account balance:", weiToString(balanceEnd));
+    console.log("Cost to deploy:", weiToString(balanceBegin.sub(balanceEnd)));
 }
 
 main()
