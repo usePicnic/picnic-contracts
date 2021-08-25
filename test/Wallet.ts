5726a8a1203c9116cd1513e0ff5959b76e5fc4d6
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-08-25 12:22:58 -0300

    [CHECKPOINT] Re-writing Wallet tests to be unit tests. Will need to create separate tests for Aave and Uniswap :D

--- test/Wallet.ts
+++ test/Wallet.ts
@@ -1,10 +1,8 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import {expect} from "chai";
+import {ethers} from "hardhat";
 
-import { getFirstEvent } from "./utils";
+import {getFirstEvent} from "./utils";
 import constants from "../constants";
-
-
 
 
 describe("Wallet", function () {
@@ -39,12 +37,11 @@ describe("Wallet", function () {
         await wallet.deployed();
     });
 
-    it("Buy DAI on Uniswap and deposit on Aave", async function () {
+    it("Writes (Buys DAI on Uniswap)", async function () {
 
         // Set bridges addresses
         var _bridgeAddresses = [
             uniswapV2SwapBridge.address,
-            aaveV2DepositBridge.address,
         ];
 
         // Set path
@@ -64,13 +61,6 @@ describe("Wallet", function () {
                     pathUniswap
                 ],
             ),
-            aaveV2DepositBridge.interface.encodeFunctionData(
-                "deposit",
-                [
-                    TOKENS['DAI'],
-                    100000
-                ]
-            )
         ];
 
         // Transfer money to wallet (similar as IndexPool contract would have done)
@@ -89,45 +79,16 @@ describe("Wallet", function () {
         // Wallet DAI amount should be 0
         let dai = await ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", TOKENS["DAI"])
         let daiBalance = await dai.balanceOf(wallet.address);
-        expect(daiBalance).to.be.equal(0);
-
-        // Wallet amDAI amount should greater than 0
-        let amDai = await ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", TOKENS["amDAI"])
-        let amDaiBalance = await amDai.balanceOf(wallet.address);
-        expect(amDaiBalance).to.be.above(0);
-
-        // Get event data
-        var event = await getFirstEvent({ address: wallet.address }, UniswapV2SwapBridge, 'TradedFromETHToTokens');
-
-        expect(event.args.path).to.eql(pathUniswap);
-        expect(event.args.amounts).to.be.an('array');
+        expect(daiBalance).to.be.above(0);
     })
 
-    it("Buys DAI on Uniswap -> Sell DAI on Uniswap", async function () {
+    it("Withdraws Ether", async function () {
 
         // Set bridges addresses
-        var _bridgeAddresses = [
-            uniswapV2SwapBridge.address
-        ];
-
-        // Set path
-        var pathUniswap = [
-            TOKENS['WMAIN'],
-            TOKENS['DAI'],
-        ];
+        var _bridgeAddresses = [];
 
         // Set encoded calls
-        var _bridgeEncodedCalls = [
-            uniswapV2SwapBridge.interface.encodeFunctionData(
-                "tradeFromETHToTokens",
-                [
-                    ADDRESSES['UNISWAP_V2_ROUTER'],
-                    100000,
-                    1,
-                    pathUniswap
-                ],
-            )
-        ];
+        var _bridgeEncodedCalls = [];
 
         // Transfer money to wallet (similar as IndexPool contract would have done)
         const transactionHash = await owner.sendTransaction({
@@ -136,158 +97,49 @@ describe("Wallet", function () {
         });
         await transactionHash.wait();
 
-        // Execute bridge calls (buys DAI on Uniswap)
-        await wallet.write(
-            _bridgeAddresses,
-            _bridgeEncodedCalls,
+        // Execute bridge calls (buys DAI on Uniswap and deposit on Aave)
+        await wallet.withdraw(
+            {'tokens': [], 'amounts': []},
+            100000,
+            owner.address
         );
 
-        // Wallet DAI amount greater than  0
-        let dai = await ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", TOKENS["DAI"])
-        let daiBalance = await dai.balanceOf(wallet.address);
-        expect(daiBalance).to.be.above(0);
-
-        var event = await getFirstEvent({ address: wallet.address }, UniswapV2SwapBridge, 'TradedFromETHToTokens');
-
-        expect(event.args.path).to.eql(pathUniswap);
-        expect(event.args.amounts).to.be.an('array');
-
-        pathUniswap = [
-            TOKENS['DAI'],
-            TOKENS['WMAIN'],
-        ]
-
-        var _bridgeEncodedCalls = [
-            uniswapV2SwapBridge.interface.encodeFunctionData(
-                "tradeFromTokensToETH",
-                [
-                    ADDRESSES['UNISWAP_V2_ROUTER'],
-                    100000,
-                    1,
-                    pathUniswap
-                ],
-            )
-        ];
-
-        await wallet.write(
-            _bridgeAddresses,
-            _bridgeEncodedCalls
-        );
-
-        event = await getFirstEvent({ address: wallet.address }, UniswapV2SwapBridge, 'TradedFromTokensToETH');
-
-        // TODO: Need to test value by checking balance
-        // TODO: Need to check amount numbers
-        expect(event.args.path).to.eql(pathUniswap);
-        expect(event.args.amounts).to.be.an('array');
+        // TODO
+        // Wallet DAI amount should be 0
+        // let dai = await ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", TOKENS["DAI"])
+        // let daiBalance = await dai.balanceOf(wallet.address);
+        // expect(daiBalance).to.be.above(0);
     })
 
-    it("Buys DAI on Uniswap and deposit on Aave and withdraw on Aave", async function () {
-        var _bridgeAddresses = [
-            uniswapV2SwapBridge.address,
-            aaveV2DepositBridge.address,
-            aaveV2DepositBridge.address,
-        ];
-        var _bridgeEncodedCalls = [
-            uniswapV2SwapBridge.interface.encodeFunctionData(
-                "tradeFromETHToTokens",
-                [
-                    ADDRESSES['UNISWAP_V2_ROUTER'],
-                    100000,
-                    1,
-                    [
-                        TOKENS['WMAIN'],
-                        TOKENS['DAI'],
-                    ]
-                ],
-            ),
-            aaveV2DepositBridge.interface.encodeFunctionData(
-                "deposit",
-                [
-                    TOKENS['DAI'],
-                    100000
-                ]
-            ),
-            aaveV2DepositBridge.interface.encodeFunctionData(
-                "withdraw",
-                [
-                    TOKENS['DAI'],
-                    100000
-                ]
-            )
-        ];
+    it("Withdraws ERC20 Tokens", async function () {
+        // TODO
+        // Set empty bridges addresses and  encoded calls
+        var _bridgeAddresses = [];
+        var _bridgeEncodedCalls = [];
 
+        // Transfer money to wallet (similar as IndexPool contract would have done)
         const transactionHash = await owner.sendTransaction({
             to: wallet.address,
             value: ethers.utils.parseEther("1"), // Sends exactly 1.0 ether
         });
-
         await transactionHash.wait();
 
-        await wallet.write(
-            _bridgeAddresses,
-            _bridgeEncodedCalls,
+        // Execute bridge calls (buys DAI on Uniswap and deposit on Aave)
+        await wallet.withdraw(
+            {'tokens': [], 'amounts': []},
+            100000,
+            owner.address
         );
 
-        var event = await getFirstEvent({ address: wallet.address }, UniswapV2SwapBridge, 'TradedFromETHToTokens');
-        
-        // expect(event.args.amounts[0]).to.equal(ethers.utils.parseEther("1"));
-        // expect(event.args.path).to.eql([ TOKENS['WMAIN'], TOKENS['DAI'] ]);
-        // expect(event.args.amounts).to.be.an('array');
-
-        event = await getFirstEvent({ address: wallet.address }, AaveV2DepositBridge, 'Deposit');
-
-        // TODO: Check amount
-        // expect(event.args.asset).to.equal(TOKENS['DAI']);
-        // expect(event.args.amount).to.equal(ethers.utils.parseEther("1"));
-
-        // TODO: Check amount
-        // TODO: Check claimed reward
-        event = await getFirstEvent({ address: wallet.address }, AaveV2DepositBridge, 'Withdraw');
-        // expect(event.args.asset).to.equal(TOKENS['DAI']);
+        // // Wallet DAI amount should be 0
+        // let dai = await ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", TOKENS["DAI"])
+        // let daiBalance = await dai.balanceOf(wallet.address);
+        // expect(daiBalance).to.be.above(0);
     })
 
     it("Rejects write from other user", async function () {
-        var _bridgeAddresses = [
-            uniswapV2SwapBridge.address,
-            aaveV2DepositBridge.address,
-            aaveV2DepositBridge.address,
-        ];
-        var _bridgeEncodedCalls = [
-            uniswapV2SwapBridge.interface.encodeFunctionData(
-                "tradeFromETHToTokens",
-                [
-                    ADDRESSES['UNISWAP_V2_ROUTER'],
-                    100000,
-                    1,
-                    [
-                        TOKENS['WMAIN'],
-                        TOKENS['DAI'],
-                    ]
-                ],
-            ),
-            aaveV2DepositBridge.interface.encodeFunctionData(
-                "deposit",
-                [
-                    TOKENS['DAI'],
-                    100000
-                ]
-            ),
-            aaveV2DepositBridge.interface.encodeFunctionData(
-                "withdraw",
-                [
-                    TOKENS['DAI'],
-                    100000
-                ]
-            )
-        ];
-
-        const transactionHash = await owner.sendTransaction({
-            to: wallet.address,
-            value: ethers.utils.parseEther("1"), // Sends exactly 1.0 ether
-        });
-
-        await transactionHash.wait();
+        var _bridgeAddresses = [];
+        var _bridgeEncodedCalls = [];
 
         await expect(wallet.connect(other).write(
             _bridgeAddresses,
@@ -295,51 +147,11 @@ describe("Wallet", function () {
         )).to.be.revertedWith("WALLET: ONLY WALLET OWNER CAN CALL THIS FUNCTION");
     })
 
-    it("Rejects failed withdraw (no collateral available)", async function () {
-        var _bridgeAddresses = [
-            uniswapV2SwapBridge.address,
-            aaveV2DepositBridge.address,
-            aaveV2DepositBridge.address,
-        ];
-        var _bridgeEncodedCalls = [
-            uniswapV2SwapBridge.interface.encodeFunctionData(
-                "tradeFromETHToTokens",
-                [
-                    ADDRESSES['UNISWAP_V2_ROUTER'],
-                    100000,
-                    1,
-                    [
-                        TOKENS['WMAIN'],
-                        TOKENS['DAI'],
-                    ]
-                ],
-            ),
-            aaveV2DepositBridge.interface.encodeFunctionData(
-                "deposit",
-                [
-                    TOKENS['QUICK'],
-                    100000
-                ]
-            ),
-            aaveV2DepositBridge.interface.encodeFunctionData(
-                "withdraw",
-                [
-                    TOKENS['QUICK'],
-                    100000
-                ]
-            )
-        ];
-
-        const transactionHash = await owner.sendTransaction({
-            to: wallet.address,
-            value: ethers.utils.parseEther("1"), // Sends exactly 1.0 ether
-        });
-
-        await transactionHash.wait();
-
-        await expect(wallet.write(
-            _bridgeAddresses,
-            _bridgeEncodedCalls,
-        )).to.be.revertedWith("revert 1"); // revert 1 means no collateral available
+    it("Rejects withdrawal from other user", async function () {
+        await expect(wallet.connect(other).withdraw(
+            {'tokens': [], 'amounts': []},
+            0,
+            owner.address
+        )).to.be.revertedWith("WALLET: ONLY WALLET OWNER CAN CALL THIS FUNCTION");
     })
 });
