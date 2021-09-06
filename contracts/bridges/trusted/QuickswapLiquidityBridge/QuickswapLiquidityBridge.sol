56044912130415d6bc8845c105082b56f8f1da07
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-09-06 19:35:44 -0300

    [REFACTOR] Adding documentation to QuickSwapLiquidityBridge

--- contracts/bridges/trusted/QuickswapLiquidityBridge/QuickswapLiquidityBridge.sol
+++ contracts/bridges/trusted/QuickswapLiquidityBridge/QuickswapLiquidityBridge.sol
@@ -5,41 +5,32 @@ import '@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol';
 import "@uniswap/v2-periphery/contracts/interfaces/IERC20.sol";
 import "../../interfaces/ILiquidity.sol";
 
-// TODO this bridge is work in progress
-
+/**
+ * @title QuickswapLiquidityBridge
+ * @author IndexPool
+ *
+ * @notice Swaps using the Quickswap contract in Polygon.
+ *
+ * @dev This contract adds liquidity to Quickswap through 2 functions:
+ *
+ * 1. addLiquidity works with 2 ERC20 tokens
+ * 2. addLiquidityETH works with 1 ERC20 token plus ETH
+ *
+ */
 contract QuickswapLiquidityBridge is ILiquidity {
 
     address constant routerAddress = 0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff;
     address constant factoryAddress = 0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32;
 
-    function addLiquidityETH(
-        uint256 ethPercentage,
-        uint256 minAmountEth,
-        address[] calldata tokens,
-        uint256[] calldata percentages,
-        uint256[] calldata minAmounts
-    ) external override {
-
-        IUniswapV2Router02 _uniswapRouter = IUniswapV2Router02(routerAddress);
-        uint256 amountToken = IERC20(tokens[0]).balanceOf(address(this)) * percentages[0] / 100000;
-
-        // Approve 0 first as a few ERC20 tokens are requiring this pattern.
-        IERC20(tokens[0]).approve(routerAddress, 0);
-        IERC20(tokens[0]).approve(routerAddress, amountToken);
-
-        _uniswapRouter.addLiquidityETH{
-        value : address(this).balance * ethPercentage / 100000}(
-            tokens[0], //       address token,
-            amountToken, //       uint amountTokenDesired,
-            minAmounts[0], //        uint amountTokenMin,
-            minAmountEth, //        uint amountETHMin,
-            address(this), //  address to,
-            block.timestamp + 100000  //   uint deadline
-        );
-
-        //emit AddLiquidityFromETH (ethAmount, assetIn, amountIn, assetOut, amountOut);
-    }
-
+    /**
+      * @notice Adds liquidity from 2 ERC20 tokens
+      *
+      * @dev Wraps add liquidity and generate the necessary events to communicate with IndexPool's UI and back-end.
+      *
+      * @param tokens List of two - token that will have liquidity added to pool
+      * @param percentages List of two - percentages of the balance of ERC20 tokens that will be added to the pool
+      * @param minAmounts List of two - minimum amounts of the ERC20 tokens required to add liquidity
+      */
     function addLiquidity(
         address[] calldata tokens,
         uint256[] calldata percentages,
@@ -72,5 +63,45 @@ contract QuickswapLiquidityBridge is ILiquidity {
 
         // todo percentages to amounts
         emit IndexPool_Liquidity_Add(tokens, percentages, assetOut, liquidity);
+    }
+
+    /**
+      * @notice Adds liquidity from ETH and ERC20 token
+      *
+      * @dev Wraps add liquidity and generate the necessary events to communicate with IndexPool's UI and back-end.
+      *
+      * @param ethPercentage Percentage of the balance of input ETH (Matic) that will be added to liquidity pool
+      * @param minAmountEth Minimum amount of the input token required to add liquidity
+      * @param tokens List of one - token that will have liquidity added alongside ETH (Matic)
+      * @param percentages List of one - percentage of the balance of the ERC20 token that will be added to pool
+      * @param minAmounts List of one - minimum amount of the ERC20 token required to add liquidity
+      */
+    function addLiquidityETH(
+        uint256 ethPercentage,
+        uint256 minAmountEth,
+        address[] calldata tokens,
+        uint256[] calldata percentages,
+        uint256[] calldata minAmounts
+    ) external override {
+
+        IUniswapV2Router02 _uniswapRouter = IUniswapV2Router02(routerAddress);
+        uint256 amountToken = IERC20(tokens[0]).balanceOf(address(this)) * percentages[0] / 100000;
+
+        // Approve 0 first as a few ERC20 tokens are requiring this pattern.
+        IERC20(tokens[0]).approve(routerAddress, 0);
+        IERC20(tokens[0]).approve(routerAddress, amountToken);
+
+        _uniswapRouter.addLiquidityETH{
+        value : address(this).balance * ethPercentage / 100000}(
+            tokens[0], //       address token,
+            amountToken, //       uint amountTokenDesired,
+            minAmounts[0], //        uint amountTokenMin,
+            minAmountEth, //        uint amountETHMin,
+            address(this), //  address to,
+            block.timestamp + 100000  //   uint deadline
+        );
+
+        // TODO emit event and fix stack too deep error
+        //emit AddLiquidityFromETH (ethAmount, assetIn, amountIn, assetOut, amountOut);
     }
 }
