409d378d256e75ac2256abbdd433b9aed1543a01
João Alexandre Vaz Ferreira <joao.avf@gmail.com>
2021-09-09 20:29:50 -0300

    [REFACTOR] IWrap interface

--- contracts/bridges/trusted/WMaticBridge/WMaticBridge.sol
+++ contracts/bridges/trusted/WMaticBridge/WMaticBridge.sol
@@ -2,25 +2,18 @@ pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IWMatic.sol";
+import "../../interfaces/IWrap.sol";
 
-contract WMaticBridge {
+contract WMaticBridge is IWrap {
     address constant wMaticAddress = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
     IWMatic constant wmatic = IWMatic(wMaticAddress);
 
-    event INDEXPOOL_WRAP_IN (
-        uint256 amountIn
-    );
-
-    event INDEXPOOL_WRAP_OUT (
-        uint256 amountOut
-    );
-
-    function wrap(uint256 percentageIn) external {
+    function wrap(uint256 percentageIn) external override {
         emit INDEXPOOL_WRAP_IN(address(this).balance * percentageIn / 100000);
         wmatic.deposit{value : address(this).balance * percentageIn / 100000}();
     }
 
-    function unwrap(uint256 percentageOut) external {
+    function unwrap(uint256 percentageOut) external override {
         emit INDEXPOOL_WRAP_OUT(IERC20(wMaticAddress).balanceOf(address(this)) * percentageOut / 100000);
         wmatic.withdraw(IERC20(wMaticAddress).balanceOf(address(this)) * percentageOut / 100000);
     }
