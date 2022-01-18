85b7800aa20b7fa79166cc3ebdc011797a5049d4
Tadashi <htadashi@defibasket.org>
2022-01-18 18:51:13 -0300

    Removed unused functions from Curve interface and added functions for liquidity bridge

--- contracts/bridges/Curve/interfaces/ICurveBasePool.sol
+++ contracts/bridges/Curve/interfaces/ICurveBasePool.sol
@@ -4,18 +4,17 @@ pragma solidity ^0.8.6;
 // Based on https://github.com/curvefi/curve-contract/blob/master/contracts/pool-templates/y/SwapTemplateY.vy
 interface ICurveBasePool {
 
-    function exchange(
-        int128 i,
-        int128 j,
-        uint256 _dx,
-        uint256 _min_dy
+    function add_liquidity(
+        uint256[] calldata _amounts,
+        uint256 _min_mint_amount        
     ) external returns (uint256);
-    
-    function exchange_underlying(
-        int128 i,
-        int128 j,
-        uint256 _dx,
-        uint256 _min_dy
-    ) external returns (uint256);
+
+    function remove_liquidity(
+        uint256 _amount,
+        uint256[] calldata _min_amouts
+    ) external returns (uint256[] memory);
+
+    function lp_token() external view returns (address);
+    function reward_receiver() external view returns (address);
 
 }
