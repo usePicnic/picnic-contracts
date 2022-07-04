01ab7a8a5f6242d45176837f846ff5eaee7d9349
João Alexandre Vaz Ferreira <30525975+joaoavf@users.noreply.github.com>
2022-07-04 18:00:41 +0000

    Making TxSimulator receive NFTs

--- contracts/TxSimulator.sol
+++ contracts/TxSimulator.sol
@@ -3,13 +3,14 @@
 pragma solidity ^0.8.6;
 import "./DeFiBasket.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 
-contract TxSimulator {
+contract TxSimulator is IERC721Receiver {
     address defiBasketAddress = 0xee13C86EE4eb1EC3a05E2cc3AB70576F31666b3b;
     DeFiBasket defiBasket = DeFiBasket(defiBasketAddress);
 
-    function createPortfolio(
+    function singleTx(
         DBDataTypes.TokenData calldata inputs,
         address[] calldata bridgeAddresses,
         bytes[] calldata bridgeEncodedCalls,
@@ -20,5 +21,10 @@ contract TxSimulator {
     uint256 tokenCounter = defiBasket.tokenCounter();
     address walletAddress = defiBasket.walletOf(tokenCounter - 1);
     return IERC20(outputToken).balanceOf(walletAddress); 
+    }
+
+
+    function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
+        return this.onERC721Received.selector;
     }
 }
