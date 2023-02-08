7a1785a350be7fe0f9e9bbc0ab089960a66fe3e3
João Alexandre Vaz Ferreira <30525975+joaoavf@users.noreply.github.com>
2023-02-08 23:35:59 +0000

    Very raw version of bridge :)

---
+++ contracts/bridges/OneInch/OneInchBridge.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.8.6;
+
+import "@uniswap/v2-periphery/contracts/interfaces/IERC20.sol";
+
+interface IAggregationExecutor {
+    /// @notice propagates information about original msg.sender and executes arbitrary data
+    function execute(address msgSender) external payable;  // 0x4b64e492
+}
+
+interface OneInchInterface {
+    /// @notice propagates information about original msg.sender and executes arbitrary data
+     function swap(
+            IAggregationExecutor executor,
+            SwapDescription calldata desc,
+            bytes calldata permit,
+            bytes calldata data
+        )
+        external
+        payable
+        returns (
+            uint256 returnAmount,
+            uint256 spentAmount
+        );  
+}
+struct SwapDescription {
+        IERC20 srcToken;
+        IERC20 dstToken;
+        address payable srcReceiver;
+        address payable dstReceiver;
+        uint256 amount;
+        uint256 minReturnAmount;
+        uint256 flags;
+    }
+
+contract OneInchBridge {        
+    function swap(
+            address oneInchAddress,
+            IAggregationExecutor executor,
+            SwapDescription calldata desc,
+            bytes calldata permit,
+            bytes calldata data
+        )
+        external
+        {
+            uint256 amount = desc.srcToken.balanceOf(address(this));
+
+            SwapDescription memory updatedDescription = SwapDescription({
+                srcToken: desc.srcToken,
+                dstToken: desc.dstToken,
+                srcReceiver: desc.srcReceiver,
+                dstReceiver: desc.dstReceiver,
+                amount: amount,
+                minReturnAmount: 1,
+                flags: desc.flags
+            });
+
+            OneInchInterface oneInch = OneInchInterface(oneInchAddress);
+            oneInch.swap(
+                executor,
+                updatedDescription,
+                permit,
+                data
+            );
+        }
+    }
