2ae0da8288a4601e3e48dc5dfa2141aa7ec7b35f
João Alexandre Vaz Ferreira <30525975+joaoavf@users.noreply.github.com>
2023-02-09 12:57:17 +0000

    running linter on 1inch files :D

--- contracts/bridges/OneInch/OneInchBridge.sol
+++ contracts/bridges/OneInch/OneInchBridge.sol
@@ -3,40 +3,37 @@ pragma solidity ^0.8.6;
 import "./interfaces/OneInchInterfaces.sol";
 import "../interfaces/IOneInchBridge.sol";
 
-contract OneInchBridge is IOneInchBridge {        
+contract OneInchBridge is IOneInchBridge {
     function swap(
-            address oneInchAddress,
-            uint256 minReturnAmount,
-            IAggregationExecutor executor,
-            SwapDescription calldata desc,
-            bytes calldata permit,
-            bytes calldata data
-        )
-        external
-        override
-        {
-            uint256 amount = desc.srcToken.balanceOf(address(this));
-            desc.srcToken.approve(oneInchAddress, amount);
+        address oneInchAddress,
+        uint256 minReturnAmount,
+        IAggregationExecutor executor,
+        SwapDescription calldata desc,
+        bytes calldata permit,
+        bytes calldata data
+    ) external override {
+        uint256 amount = desc.srcToken.balanceOf(address(this));
+        desc.srcToken.approve(oneInchAddress, amount);
 
-            SwapDescription memory updatedDescription = SwapDescription({
-                srcToken: desc.srcToken,
-                dstToken: desc.dstToken,
-                srcReceiver: desc.srcReceiver,
-                dstReceiver: payable(address(this)),
-                amount: amount,
-                minReturnAmount: minReturnAmount, // This needs to be improved eventually
-                flags: desc.flags
-            });
-           
-            OneInchInterface oneInch = OneInchInterface(oneInchAddress);
-            
-            (uint256 returnAmount, uint256 spentAmount) = oneInch.swap(
-                executor,
-                updatedDescription,
-                permit,
-                data
-            );
+        SwapDescription memory updatedDescription = SwapDescription({
+            srcToken: desc.srcToken,
+            dstToken: desc.dstToken,
+            srcReceiver: desc.srcReceiver,
+            dstReceiver: payable(address(this)),
+            amount: amount,
+            minReturnAmount: minReturnAmount, // This needs to be improved eventually
+            flags: desc.flags
+        });
 
-            emit DEFIBASKET_ONEINCH_SWAP(spentAmount, returnAmount);
-        }
+        OneInchInterface oneInch = OneInchInterface(oneInchAddress);
+
+        (uint256 returnAmount, uint256 spentAmount) = oneInch.swap(
+            executor,
+            updatedDescription,
+            permit,
+            data
+        );
+
+        emit DEFIBASKET_ONEINCH_SWAP(spentAmount, returnAmount);
     }
+}
--- contracts/bridges/OneInch/interfaces/OneInchInterfaces.sol
+++ contracts/bridges/OneInch/interfaces/OneInchInterfaces.sol
@@ -2,31 +2,25 @@ import "@uniswap/v2-periphery/contracts/interfaces/IERC20.sol";
 
 interface IAggregationExecutor {
     /// @notice propagates information about original msg.sender and executes arbitrary data
-    function execute(address msgSender) external payable;  // 0x4b64e492
+    function execute(address msgSender) external payable; // 0x4b64e492
 }
 
 interface OneInchInterface {
     /// @notice propagates information about original msg.sender and executes arbitrary data
-     function swap(
-            IAggregationExecutor executor,
-            SwapDescription calldata desc,
-            bytes calldata permit,
-            bytes calldata data
-        )
-        external
-        payable
-        returns (
-            uint256 returnAmount,
-            uint256 spentAmount
-        );  
+    function swap(
+        IAggregationExecutor executor,
+        SwapDescription calldata desc,
+        bytes calldata permit,
+        bytes calldata data
+    ) external payable returns (uint256 returnAmount, uint256 spentAmount);
 }
 
 struct SwapDescription {
-        IERC20 srcToken;
-        IERC20 dstToken;
-        address payable srcReceiver;
-        address payable dstReceiver;
-        uint256 amount;
-        uint256 minReturnAmount;
-        uint256 flags;
-    }
+    IERC20 srcToken;
+    IERC20 dstToken;
+    address payable srcReceiver;
+    address payable dstReceiver;
+    uint256 amount;
+    uint256 minReturnAmount;
+    uint256 flags;
+}
