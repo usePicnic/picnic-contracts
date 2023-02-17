649a124418b7babea0184fbff656643bb2b77fd9
João Alexandre Vaz Ferreira <30525975+joaoavf@users.noreply.github.com>
2023-02-17 20:25:40 +0000

    Fixing approve bug on TxSimulator

--- contracts/TxSimulator.sol
+++ contracts/TxSimulator.sol
@@ -20,6 +20,7 @@ contract TxSimulator is IERC721Receiver {
         bytes[] calldata bridgeEncodedCalls,
         address outputToken
     ) external returns (uint256) {
+        IERC20(inputs.tokens[0]).approve(defiBasketAddress, inputs.amounts[0]);
         defiBasket.createPortfolio(inputs, bridgeAddresses, bridgeEncodedCalls);
         uint256 tokenCounter = defiBasket.tokenCounter();
         address walletAddress = defiBasket.walletOf(tokenCounter - 1);
