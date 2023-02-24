94156e5dd5ec35bf997cf21cbc44c159bb93d846
André Thiessen <85852704+andre-thiessen@users.noreply.github.com>
2023-02-24 19:09:23 +0000

    refactor ParaswapBridge contract

--- contracts/bridges/Paraswap/ParaswapBridge.sol
+++ contracts/bridges/Paraswap/ParaswapBridge.sol
@@ -1,54 +1,11 @@
 pragma solidity ^0.8.6;
 
-import "@uniswap/v2-periphery/contracts/interfaces/IERC20.sol";
-
-struct SimpleData {
-    address fromToken;
-    address toToken;
-    uint256 fromAmount;
-    uint256 toAmount;
-    uint256 expectedAmount;
-    address[] callees;
-    bytes exchangeData;
-    uint256[] startIndexes;
-    uint256[] values;
-    address payable beneficiary;
-    address payable partner;
-    uint256 feePercent;
-    bytes permit;
-    uint256 deadline;
-    bytes16 uuid;
-}
-
-interface IAggregationExecutor {
-    /// @notice propagates information about original msg.sender and executes arbitrary data
-    function execute(address msgSender) external payable; // 0x4b64e492
-}
-
-interface ParaswapInterface{
-    function simpleSwap(
-        IAggregationExecutor executor,
-        SimpleData calldata data
-    ) external returns (uint256 receivedAmount);
-}
-
-interface IParaswapBridge{
-    event DEFIBASKET_PARASWAP_SWAP(uint256 receivedAmount);
-
-    function swap(
-        address paraswapAddress,
-        IAggregationExecutor executor,
-        SimpleData calldata paraswapParams,
-        uint256 amountInPercentage
-    ) external;
-}
-
-
+import "./interfaces/ParaswapIntefaces.sol";
+import "../interfaces/IParaswapBridge.sol";
 
 contract ParaswapBridge is IParaswapBridge {
     function swap(
         address paraswapAddress,
-        IAggregationExecutor executor,
         SimpleData calldata paraswapParams,
         uint256 amountInPercentage
     ) external override {
@@ -76,7 +33,6 @@ contract ParaswapBridge is IParaswapBridge {
         ParaswapInterface paraswap = ParaswapInterface(paraswapAddress);
 
         uint256 receivedAmount = paraswap.simpleSwap(
-            executor,
             updatedDescription
         );
 
---
+++ contracts/bridges/interfaces/IParaswapBridge.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.6;
+
+import "../Paraswap/interfaces/ParaswapIntefaces.sol";
+
+interface IParaswapBridge{
+    event DEFIBASKET_PARASWAP_SWAP(uint256 receivedAmount);
+
+    function swap(
+        address paraswapAddress,
+        SimpleData calldata paraswapParams,
+        uint256 amountInPercentage
+    ) external;
+}
