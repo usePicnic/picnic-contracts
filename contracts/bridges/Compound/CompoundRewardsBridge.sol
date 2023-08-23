c19109c398f25748bdb1ad4657be8a49e8697a12
andre-thiessen <almthiessen@gmail.com>
2023-08-23 15:26:10 -0300

    fix CompoundRewardsBridge

--- contracts/bridges/Compound/CompoundRewardsBridge.sol
+++ contracts/bridges/Compound/CompoundRewardsBridge.sol
@@ -6,8 +6,10 @@ import "./interfaces/ICometRewards.sol";
 import "../interfaces/ICompoundRewards.sol";
 
 contract CompoundRewardsBridge is ICompoundRewards {
+    address constant cometRewards = 0x45939657d1CA34A8FA39A924B71D28Fe8431e581;
+
     function claim(address comet, address src, bool shouldAccrue) external {
-        ICometRewards _cometReward = ICometRewards(comet);
+        ICometRewards _cometReward = ICometRewards(cometRewards);
         _cometReward.claim(comet, src, shouldAccrue);
 
         emit DEFIBASKET_COMPOUND_REWARDS_CLAIM(comet, src, shouldAccrue);
